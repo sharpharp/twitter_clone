bd553c6e40c3ec51a91f99ff5edfc56910d1a281
Harper Holden <harp.holden@gmail.com>
2012-03-07 20:31:44 -0600

    Added a Pages Controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterClone::Application.config.secret_token = 'dca8cc8592f2dbc6a617cf7bd8cfdd4ad38d5f3b85d6535e9c8f87e8790c5b423830b0299f33da198f25590345cce2b93e00e9204f9f46314ba7a7df35947431'
